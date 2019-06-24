4db60bb744009736ab802786ccafeeb002292196
naokita050 <49198895+naokita050@users.noreply.github.com>
2019-06-25 00:32:41 +0900

    WIP: Actorの実装

--- up-and-running/src/main/scala/com/example/BoxOffice.scala
+++ up-and-running/src/main/scala/com/example/BoxOffice.scala
@@ -2,11 +2,44 @@ package com.example
 
 import akka.actor.Actor
 import akka.util.Timeout
+import com.example.BoxOffice.{CreateEvent, EventCreated, EventExists}
 
 object BoxOffice {
+  case class CreateEvent(name: String, tickets: Int)
 
+  case class GetEvent(name: String)
+
+  case object GetEvents
+
+  case class GetTickets(event: String, tickets: Int)
+
+  case class CancelEvent(name: String)
+
+  case class Event(name: String, tickets: Int)
+
+  case class Events(events: Vector[Event])
+
+
+  sealed trait EventResponse
+
+  case class EventCreated(event: Event) extends EventResponse
+
+  case object EventExists extends EventResponse
 }
 
-class BoxOffice(implicit timeout: Timeout) extends Actor {
-  override def receive: Receive = ???
+class BoxOffice(implicit timeOut: Timeout) extends Actor {
+
+  def createTicketSeller(name: String) = context.actorOf(TicketSeller.props(name), name)
+  override def receive: Receive = {
+    case CreateEvent(name, tickets) =>
+      def create() = {
+        val eventTickets = createTicketSeller(name)
+        val newTickets = (1 to tickets).map { ticketId =>
+          TicketSeller.Ticket(ticketId)
+        }.toVector
+        eventTickets ! TicketSeller.Add(newTickets)
+        sender() ! EventCreated
+      }
+      context.child(name).fold(create())(_ => sender()! EventExists)
+  }
 }
